d00dacca15e0856e22511d494d0f84e125c4a3db
Amber C <amberpomar@gmail.com>
2023-02-07 15:03:24 +0100

    check3

--- POO/personTest.js
+++ POO/personTest.js
@@ -2,6 +2,7 @@ const {Person} = require('./person.js');
 
 const amber = new Person(1.72, 62);
 amber.hobb = ['Cocinar', 'Conducir'];
+// ckeck de que sirvan las funciones de person
 // amber.printAll();
 // amber.printHobb();
 
