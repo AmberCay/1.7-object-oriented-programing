eeff91b3cb0d17548d1d372114717cdd9b2b00b9
Amber C <amberpomar@gmail.com>
2023-02-09 11:35:03 +0100

    Retos condicionales

--- Typescript/personTest.ts
+++
@@ -1,9 +0,0 @@
-import { Person } from "./person";
-
-let amber = new Person('Amber', 25, 'Camino Alto');
-
-amber.printName();
-console.log(amber.yearOfBirth(2023));
-amber.setAdress('Caminito Alto')
-console.log(amber.getAdress);
- 
